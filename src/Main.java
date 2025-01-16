a21d181ffd1691287932930bb4f108fd6e56c70e
Joey_Davisson <joeydavisson9@gmail.com>
2025-01-16 15:48:22 -0500

    Done with Input routine

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)
+    {
+        boolean doneInput = false;
+
+         String ID = "";
+         String firstName = "";
+         String lastName = "";
+         String title = "";
+         String rec = "";
+         int YOB = 0;
+
+
+         ArrayList<String> people = new ArrayList<>();
+
+
+         Scanner in = new Scanner(System.in);
+
+
+         // Create a loop to input the person data
+        do {
+            ID = SafeInput.getNonZeroLenString(in, "Enter your ID [000001]");
+            firstName = SafeInput.getNonZeroLenString (in, "Enter your first name");
+            lastName = SafeInput.getNonZeroLenString (in, "Enter your last name");
+            title = SafeInput.getNonZeroLenString (in, "Enter your title");
+            YOB = SafeInput.getRangedInt(in, "Enter your year of birth", 1000, 9999);
+
+            rec = ID + "," + firstName + ", " + lastName + ", " + title + ", " + YOB;
+
+            System.out.println(rec);
+
+
+            people.add(rec);
+
+
+            doneInput = SafeInput.getYNConfirm(in, "Are you done [Y/N]");
+
+        }while (!doneInput);
+
+
+
+
+
+
+
+
+
+
+        // Create a loop to input the person data
+
+
     }
 }
